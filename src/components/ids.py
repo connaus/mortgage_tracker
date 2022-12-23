d0a7d2da1a9d0adeb8efc1542e1b2f42b304b25f
connaus <connaus@tcd.ie>
2022-12-23 20:42:23 +0000

    next an previous buttons working

--- src/components/ids.py
+++ src/components/ids.py
@@ -3,7 +3,9 @@ DATA_TYPE_DROPDOWN = "data-type-dropdown"
 
 MORTGAGE_AGREEMENT_MODAL = "mortgage-agreement-modal"
 MORTGAGE_AGREEMENT_MODAL_OPEN = "mortgage-agreement-modal-open"
-MORTGAGE_AGREEMENT_MODAL_CLOSE = "mortgage-agreement-modal-close"
-MORTGAGE_AGREEMENT_MODAL_NEXT = "mortgage-agreement-modal-close"
+MORTGAGE_AGREEMENT_MODAL_BODY = "mortgage-agreement-modal-body"
+MORTGAGE_AGREEMENT_MODAL_PREVIOUS = "mortgage-agreement-modal-previous"
+MORTGAGE_AGREEMENT_MODAL_NEXT = "mortgage-agreement-modal-next"
+MORTGAGE_AGREEMENT_MODAL_EDIT = "mortgage-agreement-modal-edit"
 
 LINE_CHART = "line-chart"
--- src/components/modal.py
+++ src/components/modal.py
@@ -3,23 +3,9 @@ import dash_bootstrap_components as dbc
 from . import ids
 from ..mortgage_data import MortgageAgreement
 
-i = 0
 
-
-def render(app: Dash, mortgage_list: list[MortgageAgreement]) -> html.Div:
-    @app.callback(
-        Output(ids.MORTGAGE_AGREEMENT_MODAL, "is_open"),
-        [
-            Input(ids.MORTGAGE_AGREEMENT_MODAL_OPEN, "n_clicks"),
-            # Input(ids.MORTGAGE_AGREEMENT_MODAL_CLOSE, "n_clicks"),
-        ],
-        [State(ids.MORTGAGE_AGREEMENT_MODAL, "is_open")],
-    )
-    def toggle_modal(n1, is_open) -> bool:
-        if n1:
-            return not is_open
-        return is_open
-
+def modal_children(i: int, mortgage_list: list[MortgageAgreement]) -> html.Div:
+    print("resetting modal")
     return html.Div(
         [
             dbc.Button(
@@ -30,21 +16,30 @@ def render(app: Dash, mortgage_list: list[MortgageAgreement]) -> html.Div:
             dbc.Modal(
                 [
                     dbc.ModalHeader(dbc.ModalTitle("Mortgage Details")),
-                    dbc.ModalBody(mortgage_list[i].__repr__()),
+                    dbc.ModalBody(
+                        mortgage_list[i].__repr__(),
+                        id=ids.MORTGAGE_AGREEMENT_MODAL_BODY,
+                    ),
                     dbc.ModalFooter(
                         [
                             dbc.Button(
-                                "Close",
-                                id=ids.MORTGAGE_AGREEMENT_MODAL_CLOSE,
+                                "Previous",
+                                id=ids.MORTGAGE_AGREEMENT_MODAL_PREVIOUS,
                                 class_name="ms-auto",
                                 n_clicks=0,
                             ),
-                            # dbc.Button(
-                            #     "Next",
-                            #     id=ids.MORTGAGE_AGREEMENT_MODAL_NEXT,
-                            #     class_name="ms-auto",
-                            #     n_clicks=0,
-                            # ),
+                            dbc.Button(
+                                "Edit",
+                                id=ids.MORTGAGE_AGREEMENT_MODAL_EDIT,
+                                class_name="ms-auto",
+                                n_clicks=0,
+                            ),
+                            dbc.Button(
+                                "Next",
+                                id=ids.MORTGAGE_AGREEMENT_MODAL_NEXT,
+                                class_name="ms-auto",
+                                n_clicks=0,
+                            ),
                         ]
                     ),
                 ],
@@ -54,3 +49,29 @@ def render(app: Dash, mortgage_list: list[MortgageAgreement]) -> html.Div:
             ),
         ]
     )
+
+
+def render(app: Dash, mortgage_list: list[MortgageAgreement]) -> html.Div:
+    @app.callback(
+        Output(ids.MORTGAGE_AGREEMENT_MODAL, "is_open"),
+        Input(ids.MORTGAGE_AGREEMENT_MODAL_OPEN, "n_clicks"),
+        # Input(ids.MORTGAGE_AGREEMENT_MODAL_CLOSE, "n_clicks"),
+        [State(ids.MORTGAGE_AGREEMENT_MODAL, "is_open")],
+    )
+    def toggle_modal(n1, is_open) -> bool:
+        print("I should open up more")
+        if n1:
+            return not is_open
+        return is_open
+
+    @app.callback(
+        Output(ids.MORTGAGE_AGREEMENT_MODAL_BODY, "children"),
+        [
+            Input(ids.MORTGAGE_AGREEMENT_MODAL_NEXT, "n_clicks"),
+            Input(ids.MORTGAGE_AGREEMENT_MODAL_PREVIOUS, "n_clicks"),
+        ],
+    )
+    def update_modal_text(n1: int, n2: int) -> str:
+        return mortgage_list[n1 - n2].__repr__()
+
+    return html.Div(className="modalCentre", children=modal_children(0, mortgage_list))
