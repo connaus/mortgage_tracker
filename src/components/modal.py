5a56d3e01f71233222f4af729bd1b7fec8671350
connaus <connaus@tcd.ie>
2022-12-24 09:44:04 +0000

    general code tidy. button to left of drop downs

--- src/components/modal.py
+++ src/components/modal.py
@@ -4,62 +4,13 @@ from . import ids
 from ..mortgage_data import MortgageAgreement
 
 
-def modal_children(i: int, mortgage_list: list[MortgageAgreement]) -> html.Div:
-    print("resetting modal")
-    return html.Div(
-        [
-            dbc.Button(
-                "Open Mortgage Details",
-                id=ids.MORTGAGE_AGREEMENT_MODAL_OPEN,
-                n_clicks=0,
-            ),
-            dbc.Modal(
-                [
-                    dbc.ModalHeader(dbc.ModalTitle("Mortgage Details")),
-                    dbc.ModalBody(
-                        mortgage_list[i].__repr__(),
-                        id=ids.MORTGAGE_AGREEMENT_MODAL_BODY,
-                    ),
-                    dbc.ModalFooter(
-                        [
-                            dbc.Button(
-                                "Previous",
-                                id=ids.MORTGAGE_AGREEMENT_MODAL_PREVIOUS,
-                                class_name="ms-auto",
-                                n_clicks=0,
-                            ),
-                            dbc.Button(
-                                "Edit",
-                                id=ids.MORTGAGE_AGREEMENT_MODAL_EDIT,
-                                class_name="ms-auto",
-                                n_clicks=0,
-                            ),
-                            dbc.Button(
-                                "Next",
-                                id=ids.MORTGAGE_AGREEMENT_MODAL_NEXT,
-                                class_name="ms-auto",
-                                n_clicks=0,
-                            ),
-                        ]
-                    ),
-                ],
-                id=ids.MORTGAGE_AGREEMENT_MODAL,
-                is_open=False,
-                style={"width": "25%", "display": "inline-block"},
-            ),
-        ]
-    )
-
-
 def render(app: Dash, mortgage_list: list[MortgageAgreement]) -> html.Div:
     @app.callback(
         Output(ids.MORTGAGE_AGREEMENT_MODAL, "is_open"),
         Input(ids.MORTGAGE_AGREEMENT_MODAL_OPEN, "n_clicks"),
-        # Input(ids.MORTGAGE_AGREEMENT_MODAL_CLOSE, "n_clicks"),
         [State(ids.MORTGAGE_AGREEMENT_MODAL, "is_open")],
     )
     def toggle_modal(n1, is_open) -> bool:
-        print("I should open up more")
         if n1:
             return not is_open
         return is_open
@@ -95,9 +46,55 @@ def render(app: Dash, mortgage_list: list[MortgageAgreement]) -> html.Div:
             Input(ids.MORTGAGE_AGREEMENT_MODAL_NEXT, "n_clicks"),
         ],
     )
-    def set_previous_click_limit(prev: int, next: int) -> bool:
+    def toggle_prev(prev: int, next: int) -> bool:
         if next - prev <= 0:
             return True
         return False
 
-    return html.Div(className="modalCentre", children=modal_children(0, mortgage_list))
+    return html.Div(
+        children=html.Div(
+            [
+                dbc.Button(
+                    "Open Mortgage Details",
+                    id=ids.MORTGAGE_AGREEMENT_MODAL_OPEN,
+                    n_clicks=0,
+                ),
+                dbc.Modal(
+                    [
+                        dbc.ModalHeader(dbc.ModalTitle("Mortgage Details")),
+                        dbc.ModalBody(
+                            mortgage_list[0].__repr__(),
+                            id=ids.MORTGAGE_AGREEMENT_MODAL_BODY,
+                        ),
+                        dbc.ModalFooter(
+                            [
+                                dbc.Button(
+                                    "Previous",
+                                    id=ids.MORTGAGE_AGREEMENT_MODAL_PREVIOUS,
+                                    class_name="ms-auto",
+                                    n_clicks=0,
+                                    disabled=True,
+                                ),
+                                dbc.Button(
+                                    "Edit",
+                                    id=ids.MORTGAGE_AGREEMENT_MODAL_EDIT,
+                                    class_name="ms-auto",
+                                    n_clicks=0,
+                                ),
+                                dbc.Button(
+                                    "Next",
+                                    id=ids.MORTGAGE_AGREEMENT_MODAL_NEXT,
+                                    class_name="ms-auto",
+                                    n_clicks=0,
+                                    disabled=False,
+                                ),
+                            ]
+                        ),
+                    ],
+                    id=ids.MORTGAGE_AGREEMENT_MODAL,
+                    is_open=False,
+                ),
+            ],
+        ),
+        style={"width": "25%", "display": "inline-block"},
+    )
